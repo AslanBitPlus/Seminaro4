492ab7ad2d6ea154a27b5b9d0f9f21f34f17624e
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 15:49:12 +0300

    Add T28

---
+++ T28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N.
+
+// =======================
+int Faktorial(int num)
+{
+    int result = 1;
+    for (int i =1; i <= num; i ++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+// =======================
+
+Console.WriteLine("Введите целое число:");
+Console.Write("N = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int N = Faktorial(number);
+Console.WriteLine($"Факториал числа {number} = {N}");

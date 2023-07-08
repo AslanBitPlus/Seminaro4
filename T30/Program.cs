be07da62b1d0f1555a8c1591b90434711f1b1904
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 16:47:35 +0300

    Add info T30

--- T30/Program.cs
+++ T30/Program.cs
@@ -2,19 +2,26 @@
 // заполненный нулями и единицами в случайном порядке.
 
 // =======================
-void PrintRandomArray()
+void PrintRandomArray(int num)
 {
-    int[] arrayNums = new int[8];
-    for (int i = 0; i < 8; i ++)
+    int[] arrayNums = new int[num];
+    for (int i = 0; i < arrayNums.Length; i ++)
     {
         arrayNums[i] = new Random().Next(0, 2);
+        // Console.Write($" arr[{i}]={arrayNums[i]} ");
         Console.Write($"{arrayNums[i]} ");
     }
     
 }
 // =======================
+Console.Write("Введите размерность массива :");
+int number = Convert.ToInt32(Console.ReadLine());
 
-PrintRandomArray();
+if (number > 0)
+{
+    PrintRandomArray(number);
+}
+
 
 
 

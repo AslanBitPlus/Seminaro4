e98d9c6e58c152d0ae85d71e90dc313889e87bee
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 17:57:07 +0300

    Add T29

---
+++ T29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// =======================
+void PrintRndArray(int num)
+{
+    int[] arrayNums = new int[num];
+    for (int i = 0; i < arrayNums.Length; i ++)
+    {
+        arrayNums[i] = new Random().Next(100);
+        // Console.Write($" arr[{i}]={arrayNums[i]} ");
+        Console.Write($"{arrayNums[i]} ");
+    }
+    
+}
+// =======================
+Console.Write("Введите размерность массива :");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    PrintRndArray(number);
+}
+else 
+    Console.Write("Ошибка ввода");

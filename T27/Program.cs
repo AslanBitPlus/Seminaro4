2c98a2223fc381c9ca4f9bfd55b6a1d47e39ccf4
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 17:50:30 +0300

    Add T27

---
+++ T27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// =======================
+int SumDigit(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    
+    return result;
+}
+// =======================
+
+Console.Write("Введите целое положительное число a: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int N = SumDigit(number);
+Console.WriteLine($"Сумма цифр числа {number} = {N}");

c0019678f4ead50a4d81548731b8ba0a710f61b0
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 16:25:06 +0300

    Add T30

---
+++ T30/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов
+// заполненный нулями и единицами в случайном порядке.
+
+// =======================
+void PrintRandomArray()
+{
+    int[] arrayNums = new int[8];
+    for (int i = 0; i < 8; i ++)
+    {
+        arrayNums[i] = new Random().Next(0, 2);
+        Console.Write($"{arrayNums[i]} ");
+    }
+    
+}
+// =======================
+
+PrintRandomArray();
+
+
+

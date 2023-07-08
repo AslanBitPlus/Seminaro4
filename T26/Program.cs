0cbb6774fc979e1d1854395fd15d655230f7bfc7
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 15:14:57 +0300

    Add T26

---
+++ T26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдает количество цифр в числе.
+
+// =======================
+int CountDigit(int num)
+{
+    int result = 1;
+    while (num != 0)
+    {
+        num = num / 10;
+        if (num != 0) result += 1; 
+    }
+    return result;
+}
+// =======================
+
+Console.WriteLine("Введите целое число:");
+Console.Write("N = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int N = CountDigit(number);
+Console.WriteLine($"Количество цифр в числе {number} = {N}");

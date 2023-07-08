d3f8ff82a6431aa988019a1dc2f5e70a3ea06726
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-08 17:41:44 +0300

    Add T25

---
+++ T25/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// =======================
+double Degree(int a, int b)
+{
+    double result = 1;
+     if (b > 0)
+    {
+        for (int i = 1; i <= b; i ++)
+            {
+                result = result * a; 
+            }
+    }
+    else if (b < 0)
+    {
+        b = -1 * b;
+        for (int i = 1; i <= b; i ++)
+            {
+                result = result * a; 
+            }
+        result = 1 / result;        
+    }
+    
+    
+    return result;
+}
+// =======================
+
+Console.Write("Введите целое число a: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число b: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+double N = Degree(numA, numB);
+Console.WriteLine($"{numA} в степени {numB} = {N}");
